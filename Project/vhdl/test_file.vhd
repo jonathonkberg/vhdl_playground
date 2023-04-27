bcf8baf03923682017a91fb6101bcb59223beb00
Jonathon Kreinberg <jonathonkberg@gmail.com>
2023-04-27 01:53:42 -0400

    removed finish keyword accidentally used in code

--- Project/vhdl/test_file.vhd
+++ Project/vhdl/test_file.vhd
@@ -50,10 +50,118 @@ architecture galois_expand_fsm of galois_expand is
             if (random_num_input(1) = '1') then
               for i in 7 downto 0 loop
                 mult_out(i+1) <= packet_input(i) after 1 ns;
-              end loop;
+             end loop;
             end if;
             perform_xor(1) <= '1' after 1 ns;
           end if;
         end process;
+
+
+
+            process(populate_rows) is
+      begin
+        row_0 <= "000000000000000";
+        if(populate_rows = '1') then
+          if (random_num_input(0) = '1') then
+            for i in 0 to 7 loop
+              row_0(i) <= packet_input(i);
+            end loop;
+          end if;
+          perform_xor(0) <= '1';
+        end if;
+      end process;
+
+      process(populate_rows) is
+        begin
+          row_1 <= "000000000000000";
+          if(populate_rows = '1') then
+            if (random_num_input(1) = '1') then
+              for i in 0 to 7 loop
+                row_1(i+1) <= packet_input(i);
+              end loop;
+            end if;
+            perform_xor(1) <= '1';
+          end if;
+        end process;
+
+        process(populate_rows) is
+          begin
+            row_2 <= "000000000000000";
+            if(populate_rows = '1') then
+              if (random_num_input(2) = '1') then
+                for i in 0 to 7 loop
+                  row_2(i+2) <= packet_input(i);
+                end loop;
+              end if;
+              perform_xor(2) <= '1';
+            end if;
+          end process;
+
+          process(populate_rows) is
+            begin
+              row_3 <= "000000000000000";
+              if(populate_rows = '1') then
+                if (random_num_input(3) = '1') then
+                  for i in 0 to 7 loop
+                    row_3(i+3) <= packet_input(i);
+                  end loop;
+                end if;
+                perform_xor(3) <= '1';
+              end if;
+            end process;
+
+            process(populate_rows) is
+              begin
+                row_4 <= "000000000000000";
+                if(populate_rows = '1') then
+                  if (random_num_input(4) = '1') then
+                    for i in 0 to 7 loop
+                      row_4(i+4) <= packet_input(i);
+                    end loop;
+                  end if;
+                  perform_xor(4) <= '1';
+                end if;
+              end process;
+
+              process(populate_rows) is
+                begin
+                  row_5 <= "000000000000000";
+                  if(populate_rows = '1') then
+                    if (random_num_input(5) = '1') then
+                      for i in 0 to 7 loop
+                        row_5(i+5) <= packet_input(i);
+                      end loop;
+                    end if;
+                    perform_xor(5) <= '1';
+                  end if;
+                end process;
+
+                process(populate_rows) is
+                  begin
+                    row_6 <= "000000000000000";
+                    if(populate_rows = '1') then
+                      if (random_num_input(6) = '1') then
+                        for i in 0 to 7 loop
+                          row_6(i+6) <= packet_input(i);
+                        end loop;
+                      end if;
+                      perform_xor(6) <= '1';
+                    end if;
+                  end process;
+
+                  process(populate_rows) is
+                    begin
+                      row_7 <= "000000000000000";
+                      if(populate_rows = '1') then
+                        if (random_num_input(7) = '1') then
+                          for i in 0 to 7 loop
+                            row_7(i+7) <= packet_input(i);
+                          end loop;
+                        end if;
+                        perform_xor(7) <= '1';
+                      end if;
+                    end process;
+ 
+
 
 end galois_expand_fsm;
