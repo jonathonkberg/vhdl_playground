44cc2e7fb1e9c422e685086e0dd0acc6a2bbc4e8
Jonathon Kreinberg <jonathonkberg@gmail.com>
2023-04-27 00:54:32 -0400

    removed finish keyword accidentally used in code

--- Project/vhdl/test_file.vhd
+++ Project/vhdl/test_file.vhd
@@ -1,5 +1,6 @@
-library ieee;
-use ieee.std_logic_1164.ALL;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity galois_expand is
     port(
@@ -14,7 +15,7 @@ end galois_expand;
 
 architecture galois_expand_fsm of galois_expand is
 
-  signal row_0, row_1, row_2, row_3, row_4, row_5, row_6, row_7 : std_logic_vector(15 downto 0);
+  signal row_0, row_1, row_2, row_3, row_4, row_5, row_6, row_7 : std_logic_vector(14 downto 0);
   signal perform_xor : std_logic_vector(7 downto 0);
   signal start_xor, calc_complete, populate_rows : std_logic;
     
@@ -26,14 +27,14 @@ architecture galois_expand_fsm of galois_expand is
     process(clk) is
         begin
           if (clk'event and clk = '1') then
-            row_0 <= "0000000000000000";
-            row_1 <= "0000000000000000";
-            row_2 <= "0000000000000000";
-            row_3 <= "0000000000000000";
-            row_4 <= "0000000000000000";
-            row_5 <= "0000000000000000";
-            row_6 <= "0000000000000000";
-            row_7 <= "0000000000000000";
+            row_0 <= "000000000000000";
+            row_1 <= "000000000000000";
+            row_2 <= "000000000000000";
+            row_3 <= "000000000000000";
+            row_4 <= "000000000000000";
+            row_5 <= "000000000000000";
+            row_6 <= "000000000000000";
+            row_7 <= "000000000000000";
             --mult_out <= "000000000000000";
             complete <= '0';
             perform_xor <= "00000000";
@@ -47,7 +48,7 @@ architecture galois_expand_fsm of galois_expand is
         begin
           if(populate_rows = '1' and perform_xor(1) = '0') then
             if (random_num_input(1) = '1') then
-              for i in 0 to 7 loop
+              for i in 7 downto 0 loop
                 mult_out(i+1) <= packet_input(i);
               end loop;
             end if;
