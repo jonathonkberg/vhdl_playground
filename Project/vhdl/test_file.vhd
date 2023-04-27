a8699f3d5017f86333341b434d7729761e45ccb1
Jonathon Kreinberg <jonathonkberg@gmail.com>
2023-04-27 00:34:07 -0400

    removed finish keyword accidentally used in code

--- Project/vhdl/test_file.vhd
+++ Project/vhdl/test_file.vhd
@@ -5,7 +5,7 @@ entity galois_expand is
     port(
         clk : in std_logic;
         packet_input, random_num_input : in std_logic_vector(7 downto 0);
-        mult_out : out std_logic_vector(14 downto 0);
+        mult_out : out std_logic_vector(15 downto 0);
         complete : out std_logic
     );
 end galois_expand;
@@ -14,7 +14,7 @@ end galois_expand;
 
 architecture galois_expand_fsm of galois_expand is
 
-  signal row_0, row_1, row_2, row_3, row_4, row_5, row_6, row_7 : std_logic_vector(14 downto 0);
+  signal row_0, row_1, row_2, row_3, row_4, row_5, row_6, row_7 : std_logic_vector(15 downto 0);
   signal perform_xor : std_logic_vector(7 downto 0);
   signal start_xor, calc_complete, populate_rows : std_logic;
     
@@ -26,14 +26,14 @@ architecture galois_expand_fsm of galois_expand is
     process(clk) is
         begin
           if (clk'event and clk = '1') then
-            row_0 <= "000000000000000";
-            row_1 <= "000000000000000";
-            row_2 <= "000000000000000";
-            row_3 <= "000000000000000";
-            row_4 <= "000000000000000";
-            row_5 <= "000000000000000";
-            row_6 <= "000000000000000";
-            row_7 <= "000000000000000";
+            row_0 <= "0000000000000000";
+            row_1 <= "0000000000000000";
+            row_2 <= "0000000000000000";
+            row_3 <= "0000000000000000";
+            row_4 <= "0000000000000000";
+            row_5 <= "0000000000000000";
+            row_6 <= "0000000000000000";
+            row_7 <= "0000000000000000";
             --mult_out <= "000000000000000";
             complete <= '0';
             perform_xor <= "00000000";
