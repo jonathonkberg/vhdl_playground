a805771c4852499c135b1c224ea861d4663494f4
Jonathon Kreinberg <jonathonkberg@gmail.com>
2023-04-27 02:36:49 -0400

    removed finish keyword accidentally used in code

--- Project/vhdl/galois_expand.vhd
+++ Project/vhdl/galois_expand.vhd
@@ -15,7 +15,7 @@ end galois_expand;
 architecture galois_expand_fsm of galois_expand is
 
   signal row_0, row_1, row_2, row_3, row_4, row_5, row_6, row_7 : std_logic_vector(14 downto 0);
-  signal perform_xor : std_logic_vector(7 downto 0);
+  signal ready_0, ready_1, ready_2, ready_3, ready_4, ready_5, ready_6, ready_7 : std_logic;
   signal start_xor, calc_complete, populate_rows : std_logic;
     
   begin
@@ -23,22 +23,12 @@ architecture galois_expand_fsm of galois_expand is
 -- Restart expansion state machine
   -- Full expansion algorithm occurs in one main clock cycle
   -- Signals are re-initialized at restart of state machine
-    process(clk) is
+    process(clk, complete) is
         begin
           if (clk'event and clk = '1') then
-            row_0 <= "000000000000000";
-            row_1 <= "000000000000000";
-            row_2 <= "000000000000000";
-            row_3 <= "000000000000000";
-            row_4 <= "000000000000000";
-            row_5 <= "000000000000000";
-            row_6 <= "000000000000000";
-            row_7 <= "000000000000000";
-            mult_out <= "000000000000000";
-            complete <= '0';
-            perform_xor <= "00000000";
-            calc_complete <= '0';
             populate_rows <= '1';
+          elsif (complete = '1') then
+            populate_rows <= '0';
           end if;
         end process;
         
@@ -48,119 +38,128 @@ architecture galois_expand_fsm of galois_expand is
   -- Eight rows are populated concurrently
     process(populate_rows) is
       begin
+        ready_0 <= '0';
+        row_0 <= "000000000000000";
         if(populate_rows = '1') then
           if (random_num_input(0) = '1') then
             for i in 0 to 7 loop
-              row_0(i) <= packet_input(i);
+              row_0(i) <= packet_input(i) after 1 ns;
             end loop;
           end if;
-          perform_xor(0) <= '1';
+          ready_0 <= '1' after 1 ns;
         end if;
       end process;
 
       process(populate_rows) is
         begin
+          ready_1 <= '0';
+          row_1 <= "000000000000000";
           if(populate_rows = '1') then
             if (random_num_input(1) = '1') then
               for i in 0 to 7 loop
-                row_1(i+1) <= packet_input(i);
+                row_1(i+1) <= packet_input(i) after 1 ns;
               end loop;
             end if;
-            perform_xor(1) <= '1';
+            ready_1 <= '1' after 1 ns;
           end if;
         end process;
 
         process(populate_rows) is
           begin
+            ready_2 <= '0';
+            row_2 <= "000000000000000";
             if(populate_rows = '1') then
               if (random_num_input(2) = '1') then
                 for i in 0 to 7 loop
-                  row_2(i+2) <= packet_input(i);
+                  row_2(i+2) <= packet_input(i) after 1 ns;
                 end loop;
               end if;
-              perform_xor(2) <= '1';
+              ready_2 <= '1' after 1 ns;
             end if;
           end process;
 
           process(populate_rows) is
             begin
+              ready_3 <= '0';
+              row_3 <= "000000000000000";
               if(populate_rows = '1') then
                 if (random_num_input(3) = '1') then
                   for i in 0 to 7 loop
-                    row_3(i+3) <= packet_input(i);
+                    row_3(i+3) <= packet_input(i) after 1 ns;
                   end loop;
                 end if;
-                perform_xor(3) <= '1';
+                ready_3 <= '1' after 1 ns;
               end if;
             end process;
 
             process(populate_rows) is
               begin
+                ready_4 <= '0';
+                row_4 <= "000000000000000";
                 if(populate_rows = '1') then
                   if (random_num_input(4) = '1') then
                     for i in 0 to 7 loop
-                      row_4(i+4) <= packet_input(i);
+                      row_4(i+4) <= packet_input(i) after 1 ns;
                     end loop;
                   end if;
-                  perform_xor(4) <= '1';
+                  ready_4 <= '1' after 1 ns;
                 end if;
               end process;
 
               process(populate_rows) is
                 begin
+                  ready_5 <= '0';
+                  row_5 <= "000000000000000";
                   if(populate_rows = '1') then
                     if (random_num_input(5) = '1') then
                       for i in 0 to 7 loop
-                        row_5(i+5) <= packet_input(i);
+                        row_5(i+5) <= packet_input(i) after 1 ns;
                       end loop;
                     end if;
-                    perform_xor(5) <= '1';
+                    ready_5 <= '1' after 1 ns;
                   end if;
                 end process;
 
                 process(populate_rows) is
                   begin
+                    ready_6 <= '0';
+                    row_6 <= "000000000000000";
                     if(populate_rows = '1') then
                       if (random_num_input(6) = '1') then
                         for i in 0 to 7 loop
-                          row_6(i+6) <= packet_input(i);
+                          row_6(i+6) <= packet_input(i) after 1 ns;
                         end loop;
                       end if;
-                      perform_xor(6) <= '1';
+                      ready_6 <= '1' after 1 ns;
                     end if;
                   end process;
 
                   process(populate_rows) is
                     begin
+                      ready_7 <= '0';
+                      row_7 <= "000000000000000";
                       if(populate_rows = '1') then
                         if (random_num_input(7) = '1') then
                           for i in 0 to 7 loop
-                            row_7(i+7) <= packet_input(i);
+                            row_7(i+7) <= packet_input(i) after 1 ns;
                           end loop;
                         end if;
-                        perform_xor(7) <= '1';
+                        ready_7 <= '1' after 1 ns;
                       end if;
                     end process;
  
       
--- Direct FSM to XOR operation
-      process(perform_xor)
-        begin
-          if (perform_xor = "11111111") then
-            populate_rows <= '0';
-            start_xor <= '1';
-          end if;
-        end process;
         
 -- XOR operation
-      process(start_xor)
+      process(populate_rows, ready_0, ready_1, ready_2, ready_3, ready_4, ready_5, ready_6, ready_7)
         begin
-          if (start_xor = '1') then
+          mult_out(i) <= "000000000000000";
+          complete <= '0';
+          if (ready_0 = '1' and ready_1 = '1' and ready_2 = '1' and ready_3 = '1' and ready_4 = '1' and ready_5 = '1' and ready_6 = '1' and ready_7 = '1') then
             for i in 0 to 14 loop
-              mult_out(i) <= (((((((row_0(i) xor row_1(i)) xor row_2(i)) xor row_3(i)) xor row_4(i)) xor row_5(i)) xor row_6(i)) xor row_7(i));
+              mult_out(i) <= (((((((row_0(i) xor row_1(i)) xor row_2(i)) xor row_3(i)) xor row_4(i)) xor row_5(i)) xor row_6(i)) xor row_7(i)) after 1 ns;
             end loop;
-            calc_complete <= '1';
-            complete <= '1';
+            complete <= '1' after 1 ns;
           end if;
         end process;
             
